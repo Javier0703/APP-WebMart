888a28895224956abd8ffbbfd61fbc5081601261
Javier Calvo Porro <115535833+Javier0703@users.noreply.github.com>
2023-05-08 23:26:31 +0200

    Haciendo el perfil

--- WebMart/W_USER/perfil/perfil.php
+++ WebMart/W_USER/perfil/perfil.php
@@ -78,6 +78,7 @@ else{
     <link rel="shortcut icon" href="../../IMG/LOGOS_ERRORES/logo.png">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,-25"/>
     <title>WebMart</title>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin=""/>
     <link rel="stylesheet" href="../../CSS/estilos.css">
 </head>
 
@@ -118,41 +119,196 @@ else{
 
 <main class="mPerfil">
     <section>
-        <aside>
-            <div>
+        <aside id="a1">
+            <a href="perfil.php" class="aSelected">
                 <?php
                 $usu=USU;
-                $con=conexUsu();
-                $sql="SELECT ICONO,USUARIO FROM usuarios WHERE USUARIO=?";
+                $sql="SELECT ID_USU,ICONO,USUARIO,NOMBRE,APELLIDO1 FROM usuarios WHERE USUARIO=?";
                 $st=$con->prepare($sql);
                 $st->bind_param("s",$usu);
                 $st->execute();
-                $st->bind_result($icono, $usuDB);
+                $st->bind_result($id_usuDB,$icono, $usuDB,$nDB,$aDB);
                 $st->fetch();
-                echo '<div id="profileIcon"><img src="data:image/jpg;base64,'.base64_encode($icono).'"></div>';
+                $id=$id_usuDB;
+                $st->close();
+                echo '<img src="data:image/jpg;base64,'.base64_encode($icono).'">';
                 ?>
-                <p><?=$usuDB?></p>
-            </div>
-        </aside>
-        <aside>
+                <div>
+                    <p>
+                        <?php
+                            if ($nDB===null){
+                                echo $usuDB;
+                            }
+                            else{
+                                echo ucfirst($usuDB)." ".strtoupper(substr($aDB, 0, 1));
+                            }
+                        ?>
+                    </p>
+                    <p>
+                        <?php
+                        $sql="SELECT COUNT(*) OP FROM opiniones join productos p USING (ID_PROD) WHERE p.ID_USU=?";
+                        $st=$con->prepare($sql);
+                        $st->bind_param("i",$id);
+                        $st->execute();
+                        $st->bind_result($opinion);
+                        $st->fetch();
+                        if ($opinion===1){
+                            echo "1 opinión";
+                        }
+                        else if ($opinion>1){
+                            echo $opinion. "opiniones";
+                        }
+                        else{
+                            echo "Sin opiniones";
+                        }
+                        $st->close();
+                        ?>
+                    </p>
+                </div>
+            </a>
 
+            <section>
+
+                <a href="productos.php">
+                    <img src="../../IMG/ICONS_NAV/ICONOS_ASIDE/prod.png" alt="Prod">
+                    <div>
+                        <p>Productos</p>
+                    </div>
+                </a>
+
+                <a href="compras.php">
+                    <img src="../../IMG/ICONS_NAV/ICONOS_ASIDE/compras.png" alt="Compras">
+                    <div>
+                        <p>Compras</p>
+                    </div>
+                </a>
+
+                <a href="favoritos.php">
+                    <img src="../../IMG/ICONS_NAV/ICONOS_ASIDE/favoritos.png" alt="Favoritos">
+                    <div>
+                        <p>Favoritos</p>
+                    </div>
+                </a>
+
+                <a href="mensajeria/mensajes.php">
+                    <img src="../../IMG/ICONS_NAV/ICONOS_ASIDE/mensajes.png" alt="Mensajes">
+                    <div>
+                        <p>Mensajes</p>
+                    </div>
+                </a>
+
+            </section>
         </aside>
+
+        <aside id="a2">
+
+            <form action="saveProfile.php " method="POST" enctype="multipart/form-data">
+
+                <section class="sAPerfil1">
+                    <h3>Icono del perfil</h3>
+                        <div>
+                                <?php
+                                $usu=USU;
+                                $sql="SELECT ICONO FROM usuarios WHERE USUARIO=?";
+                                $st=$con->prepare($sql);
+                                $st->bind_param("s",$usu);
+                                $st->execute();
+                                $st->bind_result($icono);
+                                $st->fetch();
+                                echo '<div id="vista-previa"><img src="data:image/jpg;base64,'.base64_encode($icono).'"></div>';
+                                $st->close();
+                                ?>
+                            <input type="file" name="icono" accept="image/jpeg" id="file" onchange="mostrarImagen()">
+                            <label for="file"><span class="material-symbols-outlined">add_photo_alternate</span><p>Cambiar icono</p></label>
+                        </div>
+                    <p>El formato debe de ser jpg y es recomendable que sea un icono (misma altura y anchura) para evitar deformaciones</p>
+                </section>
+
+                <section class="sAPerfil2">
+                    <h3>Información pública</h3>
+                    <section>
+                        <div>
+                            <?php
+                            $usu=USU;
+                            $sql="SELECT NOMBRE, APELLIDO1, APELLIDO2, DESCRIPCION, CORREO, DIRECCION FROM usuarios WHERE USUARIO=?";
+                            $st=$con->prepare($sql);
+                            $st->bind_param("s",$usu);
+                            $st->execute();
+                            $st->bind_result($n, $ap1, $ap2, $desc, $correo, $dir);
+                            $st->fetch();
+                            ?>
+                            <label for="nombre">Nombre</label><br>
+                            <input type="text" id="nombre" name="nombre" value="<?=$n?>" placeholder="Nombre">
+                        </div>
+                        <div>
+                            <label for="nombre">Primer Apellido</label><br>
+                            <input type="text" id="nombre" name="ape1" value="<?=$ap1?>" placeholder="Primer apellido">
+                        </div>
+                        <div>
+                            <label for="nombre">Segundo Apellido</label><br>
+                            <input type="text" id="nombre" name="ape2" value="<?=$ap2?>" placeholder="Segundo apellido">
+                        </div>
+                        <div>
+                            <label for="nombre">Correo</label><br>
+                            <input type="text" id="nombre" name="correo" value="<?=$correo?>" placeholder="Correo">
+                        </div>
+                    </section>
+
+                    <div>
+                        <label for="descripcion">Descripción</label><br>
+                        <textarea name="descripcion" id="descripcion" cols="30" rows="10">Hola</textarea>
+                    </div>
+                </section>
+
+                <section class="sAPerfil3">
+                    <div id="map">
+                    </div>
+                </section>
+                <?php
+                $st->close();
+                ?>
+                <section class="sAPerfil2">
+
+                </section>
+
+
+            </form>
+
+            <footer class="footerProfile">
+                <div>
+                    <p>Creado por Javier Calvo Porro</p>
+                    <table>
+                        <tr>
+                            <td><img id="github" src="../../IMG/LOGOS_ERRORES/github.png" alt="Github"></td>
+                            <td>Github</td>
+                        </tr>
+                    </table>
+                </div>
+            </footer>
+        </aside>
+
     </section>
 </main>
 
-<footer>
-    <div>
-        <p>Creado por Javier Calvo Porro</p>
-        <table>
-            <tr>
-                <td><img id="github" src="../../IMG/LOGOS_ERRORES/github.png" alt="Github"></td>
-                <td>Github</td>
-            </tr>
-        </table>
-    </div>
-</footer>
-
 </body>
 <script src="../../JS_APP/header.js"></script>
+<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
+<script src="../../JS_APP/PERFIL/map.js"></script>
+<script>
+    function mostrarImagen() {
+        let input = document.getElementById('file');
+        if (input.files && input.files[0]) {
+            let reader = new FileReader();
+            reader.onload = function(e) {
+                let vistaPrevia = document.getElementById('vista-previa');
+                let imagen = new Image();
+                imagen.src = e.target.result;
+                vistaPrevia.innerHTML = '';
+                vistaPrevia.appendChild(imagen);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+</script>
 </html>
 
