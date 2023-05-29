7fed4f82d82893b76ef852c0c008b19e672a2099
Javier Calvo Porro <115535833+Javier0703@users.noreply.github.com>
2023-05-30 00:14:15 +0200

    403 error

---
+++ WebMart/403.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="IMG/LOGOS_ERRORES/logo.png">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,1,0"/>
+    <title>WebMart</title>
+    <link rel="stylesheet" href="CSS/index.css">
+</head>
+<body>
+
+<header>
+
+    <a id="logo" href="index.php"><img src="IMG/LOGOS_ERRORES/logo.png" alt="Logo"></a>
+    <a id="logoE" href="index.php"><img src="IMG/LOGOS_ERRORES/logoEntero.png" alt="Logo"></a>
+
+    <nav>
+        <ul>
+            <a href="index.php"><li>Inicio</li></a>
+            <a href="información.html"><li>Información</li></a>
+            <a href="contacto.php"><li>Contacto</li></a>
+        </ul>
+    </nav>
+
+    <img id="desplegable" class="desplegable" src="IMG/LOGOS_ERRORES/despleg.png" alt="Desplegable">
+
+</header>
+
+<main class="e404">
+    <div>
+        <img src="IMG/LOGOS_ERRORES/403.jpg" alt="404">
+        <p>Lo siento, tienes prohibido este acceso... :(</p>
+    </div>
+</main>
+
+<footer>
+    <div>
+        <p>Creado por Javier Calvo Porro</p>
+        <table>
+            <tr>
+                <td><img id="github" src="IMG/LOGOS_ERRORES/github.png" alt="Github"></td>
+                <td><a href="https://github.com/Javier0703/APP-WebMart" target="_blank">Disponible en GitHub</a></td>
+            </tr>
+        </table>
+    </div>
+</footer>
+
+</body>
+<script src="JS/index.js"></script>
+<script src="JS/openMenu.js"></script>
+</html>
